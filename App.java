fec7d03319ea8f23ea9ccc98940cec58b9f0cbc2
Rupa2004 <rupadandina@gmail.com>
2023-10-31 23:34:18 +0530

    Commited by Rupa

---
+++ App.java
@@ -0,0 +1,13 @@
+package com.srkit.Maven;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "This program is about SonarQube Server" );
+    }
+}
